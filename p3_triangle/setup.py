a53ae83980c18f5478650575f739057002e2207d
dafauu <dafaucillox2005@gmail.com>
2026-02-26 10:42:04 +0100

    Acabado

---
+++ p3_triangle/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'p3_triangle'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daviddaf',
+    maintainer_email='dafaucillox2005@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'tf_triangle_node = p3_triangle.tf_triangle_node:main'
+        ],
+    },
+)
